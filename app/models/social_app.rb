aaa044c782fbee3fff71cdcbb78f8eb4cd692adb
Srihari K <srihari@mobizion.com>
2012-11-24 18:32:59 +0530

    Create social app from omniauth hash

--- app/models/social_app.rb
+++ app/models/social_app.rb
@@ -1,4 +1,12 @@
 class SocialApp < ActiveRecord::Base
   attr_accessible :name, :provider, :uid
   belongs_to :user
+
+  def self.create_from_omniauth(auth)
+    create! do |app|
+      app.provider = auth['provider']
+      app.uid = auth['uid']
+      app.name = auth['info']['name']
+    end
+  end
 end
--- spec/models/social_app_spec.rb
+++ spec/models/social_app_spec.rb
@@ -2,4 +2,12 @@ require 'spec_helper'
 
 describe SocialApp do
   it {should belong_to(:user)}
+  describe '.create_from_omniauth' do
+    it 'should create a new social app from given hash' do
+      hash = {'provider' => 'prv','uid' => 'alongstringwithtoomany','info' => {'name' => 'srihari'}}
+      lambda do
+        social_app = SocialApp.create_from_omniauth(hash)
+      end.should change(SocialApp, :count).by(1)
+    end
+  end
 end
