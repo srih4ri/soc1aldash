036b83cba3becd7ea7fbb63e85b2e007b5857538
Srihari K <srihari@mobizion.com>
2012-11-24 12:11:54 +0530

    Initial Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SocialDash::Application.config.secret_token = 'f5f872c04ec66edcbdc76eb366e4074a095369b45a7c9a9757f5d8ca20ae06ac2844d8e8609561590a7227b20240a77a21a750e5f19116951d7aea42d9b12d30'
