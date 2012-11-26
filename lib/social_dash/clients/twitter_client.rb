7bfafca0ddd600f05fc83d653175309a3afca3bc
Srihari K <srihari@mobizion.com>
2012-11-27 01:57:56 +0530

    Add credentials as a key in settings

--- lib/social_dash/clients/twitter_client.rb
+++ lib/social_dash/clients/twitter_client.rb
@@ -1,6 +1,12 @@
 module SocialDash
   module Clients
     class TwitterClient
+
+      def initialize(social_app)
+        @credentials = social_app.settings['credentials']
+        @search_terms = social_app.settings['search_terms']
+      end
+
       def self.settings_for(oauth_response)
         oauth_response['credentials']
       end
--- spec/lib/clients/twitter_client_spec.rb
+++ spec/lib/clients/twitter_client_spec.rb
@@ -7,8 +7,8 @@ describe SocialDash::Clients::TwitterClient do
       omniauth_hash = {'provider' => :twitter,'credentials' =>
         {'token' =>  '17567838-os24N3MDQNjTnIsa4IW26SPZmpSxo2nvXWvjVb4cr',
           'secret' => 'ZhuIfIghQiLI2U8xmm1JYdd91qhpM8mQOrHBrius4z'}}
-      settings = {'token' =>  '17567838-os24N3MDQNjTnIsa4IW26SPZmpSxo2nvXWvjVb4cr',
-        'secret' => 'ZhuIfIghQiLI2U8xmm1JYdd91qhpM8mQOrHBrius4z'}
+      settings = {:credentials => {'token' =>  '17567838-os24N3MDQNjTnIsa4IW26SPZmpSxo2nvXWvjVb4cr',
+          'secret' => 'ZhuIfIghQiLI2U8xmm1JYdd91qhpM8mQOrHBrius4z'} }
       SocialDash::Clients::TwitterClient.settings_for(omniauth_hash).should eq(settings)
     end
   end
