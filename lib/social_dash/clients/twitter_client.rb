2c9cbdb7ccf5de02b70d220efd2479b909bc1b36
Srihari K <srihari@mobizion.com>
2012-11-26 22:40:01 +0530

    Twitter to store credentials required for fetching tweets

--- lib/social_dash/clients/twitter_client.rb
+++ lib/social_dash/clients/twitter_client.rb
@@ -1,8 +1,8 @@
 module SocialDash
   module Clients
     class TwitterClient
-      def self.settings_for(oauth)
-        nil
+      def self.settings_for(oauth_response)
+        oauth_response['credentials']
       end
     end
   end
---
+++ spec/lib/clients/twitter_client_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe SocialDash::Clients::TwitterClient do
+  describe '.settings_for' do
+
+    it 'should return settings from oauth hash' do
+      omniauth_hash = {'provider' => :twitter,'credentials' =>
+        {'token' =>  '17567838-os24N3MDQNjTnIsa4IW26SPZmpSxo2nvXWvjVb4cr',
+          'secret' => 'ZhuIfIghQiLI2U8xmm1JYdd91qhpM8mQOrHBrius4z'}}
+      settings = {'token' =>  '17567838-os24N3MDQNjTnIsa4IW26SPZmpSxo2nvXWvjVb4cr',
+        'secret' => 'ZhuIfIghQiLI2U8xmm1JYdd91qhpM8mQOrHBrius4z'}
+      SocialDash::Clients::TwitterClient.settings_for(omniauth_hash).should eq(settings)
+    end
+  end
+end
