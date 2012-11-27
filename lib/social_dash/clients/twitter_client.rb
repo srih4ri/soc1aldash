5abbb2ee79594da844a71f88b5497829aea8832b
Srihari K <srihari@mobizion.com>
2012-11-27 15:38:20 +0530

    Mentions and Search API for twitter client

--- lib/social_dash/clients/twitter_client.rb
+++ lib/social_dash/clients/twitter_client.rb
@@ -3,13 +3,34 @@ module SocialDash
     class TwitterClient
 
       def initialize(social_app)
-        @credentials = social_app.settings['credentials']
-        @search_terms = social_app.settings['search_terms']
+        @credentials = social_app.settings['credentials'] || {}
+        @search_terms = social_app.settings['search_terms'] || {}
       end
 
       def self.settings_for(oauth_response)
-        oauth_response['credentials']
+        {:credentials => oauth_response['credentials']}
       end
+
+      def client
+        @client ||= Twitter::Client.new(
+                                    :oauth_token => @credentials['token'],
+                                    :oauth_token_secret => @credentials['secret']
+                                    )
+      end
+
+
+      def mentions
+        client.mentions_timeline
+      end
+
+      def search_results
+        @search_terms.blank? ? [] : search(@search_terms.join(' OR '))
+      end
+
+      def search(keywords)
+        client.search(keywords)
+      end
+
     end
   end
 end
--- spec/lib/clients/twitter_client_spec.rb
+++ spec/lib/clients/twitter_client_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe SocialDash::Clients::TwitterClient do
   describe '.settings_for' do
-
     it 'should return settings from oauth hash' do
       omniauth_hash = {'provider' => :twitter,'credentials' =>
         {'token' =>  '17567838-os24N3MDQNjTnIsa4IW26SPZmpSxo2nvXWvjVb4cr',
@@ -12,4 +11,42 @@ describe SocialDash::Clients::TwitterClient do
       SocialDash::Clients::TwitterClient.settings_for(omniauth_hash).should eq(settings)
     end
   end
+
+  describe '#client' do
+    it "should return an instance of twitter gem's client'" do
+      social_app = mock(social_app)
+      social_app.stub(:settings).and_return({'credentials' => {}})
+      twt = SocialDash::Clients::TwitterClient.new(social_app)
+      twt.client.should be_instance_of(Twitter::Client)
+    end
+  end
+
+  describe '#mentions' do
+    it "should delegate mentions to twitter gem" do
+      social_app = mock(social_app)
+      social_app.stub(:settings).and_return({'credentials' => {}})
+      twt = SocialDash::Clients::TwitterClient.new(social_app)
+      Twitter::Client.any_instance.should_receive(:mentions_timeline).and_return([])
+      twt.mentions
+    end
+  end
+
+  describe '#search_results' do
+    it "should delegate search for given terms to twitter gem" do
+      social_app = mock(social_app)
+      social_app.stub(:settings).and_return({'search_terms' => ['my company','com'],'credentials' => {}})
+      twt = SocialDash::Clients::TwitterClient.new(social_app)
+      Twitter::Client.any_instance.should_receive(:search).with('my company OR com').and_return([])
+      twt.search_results
+    end
+    it "should not hit twitter API if no search terms are set" do
+      social_app = mock(social_app)
+      social_app.stub(:settings).and_return({'search_terms' => [],'credentials' => {}})
+      twt = SocialDash::Clients::TwitterClient.new(social_app)
+      Twitter::Client.any_instance.should_not_receive(:search)
+      twt.search_results
+    end
+
+  end
+
 end
