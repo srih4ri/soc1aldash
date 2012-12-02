39713b5b7455c597599254a6cb6d3e93d2a142d9
Srihari K <srihari@mobizion.com>
2012-12-03 03:48:00 +0530

    Change tests to reflect change in UI

--- spec/requests/user_registration_spec.rb
+++ spec/requests/user_registration_spec.rb
@@ -8,7 +8,7 @@ describe "user registration" do
     fill_in "user_password",              :with => "password"
     fill_in "user_password_confirmation", :with => "password"
 
-    click_button "Sign up"
+    click_button "Sign Up"
 
     page.should have_content("Welcome! You have signed up successfully.")
   end
