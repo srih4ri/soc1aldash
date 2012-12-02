fcfb744f76c11f6d28d515ecf87eeeffe0a08a31
Srihari K <srihari@mobizion.com>
2012-12-03 03:46:54 +0530

    last_fetched_at column added

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121125184347) do
+ActiveRecord::Schema.define(:version => 20121202213157) do
 
   create_table "social_apps", :force => true do |t|
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
     t.integer  "user_id"
     t.text     "settings"
+    t.datetime "last_fetched_at"
   end
 
   create_table "users", :force => true do |t|
